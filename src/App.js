309c9a3650ea70f727466b7a17637e8c44fbccde
Ahmad Sandid <ahmad1702@gmail.com>
2022-03-23 21:27:33 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import logo from './logo.svg';
+import './App.scss';
+import { Link, Routes, Route } from 'react-router-dom'
+
+//Components
+import Translation from './components/Translation/Translation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Translation />}/>
+        <Route path="*" element={<Translation />} />
+      </Routes>
     </div>
   );
 }
